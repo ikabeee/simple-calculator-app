64e65bfe5bec1f87a840af5a7546d50f6e65738f
ikabeee <carlglz30@gmail.com>
2025-03-13 06:57:38 -0500

    add division component for calculating and displaying division results

---
+++ app/division.tsx
@@ -0,0 +1,19 @@
+import { useRouter, useLocalSearchParams } from "expo-router";
+import { View, Text, Button } from "react-native";
+
+export default function Division() {
+    const router = useRouter();
+    const params = useLocalSearchParams();
+    const { firstNumber, secondNumber } = params;
+    const result = (firstNumber: string, secondNumber: string): string => {
+        const first = parseInt(firstNumber);
+        const second = parseInt(secondNumber);
+        return (first / second).toString();
+    }
+    return (
+        <View>
+            <Text>El resultado es: {!firstNumber || !secondNumber ? `No recibí todos los numeros` : result(firstNumber as string, secondNumber as string)}</Text>
+            <Button title ="Inicio" onPress={()=>router.back()}/>
+        </View>
+    )
+}
